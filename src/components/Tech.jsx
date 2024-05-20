1a65e7adc9d234da14b84958decd63c82aecb596
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-05-20 13:57:25 -0400

    media screen css for different screen sizes for tech section, tech-bubble, lol

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,71 +1,69 @@
-// const Tech = () => {
-//   return (
-//     <div id="tech" className="bottom-line">
-//       <div className="bump-it-over">
-//         <h1>Technologies</h1>
-//         <p><h5 className="major-mono">Languages:</h5>Javascript, Python</p>
-//         <p><h5 className="major-mono">Front End:</h5>React, Redux, HTML, CSS, Bootstrap CSS, Tailwind CSS, Ant Design</p>
-//         <p><h5 className="major-mono">Back End:</h5>Node.js, Express, Rest API, PostgreSQL, MySQL, Prisma ORM, BCrypt, Axios</p>
-//         <p><h5 className="major-mono">Everything Else:</h5>Phaser3, Git, GitHub, Vite, Visual Studio Code, Figma, Photoshop, Render, Wordpress, PANDAS, NumPY, Gulp, SSH/SFTP, CORS…</p>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Tech;
-
 const Tech = () => {
   return (
     <div id="tech" className="bottom-line">
       <div className="bump-it-over">
+
         <h1>Technologies</h1>
+        <section className="navigation">
 
-        <h5 className="major-mono">Languages:</h5>
-        <ul>
-          <li>Javascript</li>
-          <li>Python</li>
-        </ul>
+          <article className="tech-bubble shadow-bottom">
+            <h2 className="major-mono">Languages:</h2>
+            <ul>
+              <li>Javascript</li>
+              <li>Python</li>
+            </ul>
+          </article>
 
-        <h5 className="major-mono">Front End:</h5>
-        <ul>
-          <li>React</li>
-          <li>Redux</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Bootstrap CSS</li>
-          <li>Tailwind CSS</li>
-          <li>Ant Design</li>
-        </ul>
+          <article className="tech-bubble shadow-bottom">
+            <h2 className="major-mono">Front End:</h2>
+            <ul>
+              <li>React</li>
+              <li>Redux</li>
+              <li>React Router</li>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Bootstrap CSS</li>
+              <li>Tailwind CSS</li>
+              <li>Ant Design</li>
+            </ul>
+          </article>
 
-        <h5 className="major-mono">Back End:</h5>
-        <ul>
-          <li>Node.js</li>
-          <li>Express</li>
-          <li>Rest API</li>
-          <li>PostgreSQL</li>
-          <li>MySQL</li>
-          <li>Prisma ORM</li>
-          <li>BCrypt</li>
-          <li>Axios</li>
-        </ul>
+          <article className="tech-bubble shadow-bottom">
+            <h2 className="major-mono">Back End:</h2>
+            <ul>
+              <li>Node.js</li>
+              <li>Express</li>
+              <li>Express Router</li>
+              <li>Rest API</li>
+              <li>PostgreSQL</li>
+              <li>MySQL</li>
+              <li>Prisma ORM</li>
+              <li>BCrypt</li>
+              <li>Axios</li>
+            </ul>
+          </article>
 
-        <h5 className="major-mono">Everything Else:</h5>
-        <ul>
-          <li>Phaser3</li>
-          <li>Git</li>
-          <li>GitHub</li>
-          <li>Vite</li>
-          <li>Visual Studio Code</li>
-          <li>Figma</li>
-          <li>Photoshop</li>
-          <li>Render</li>
-          <li>Wordpress</li>
-          <li>PANDAS</li>
-          <li>NumPY</li>
-          <li>Gulp</li>
-          <li>SSH/SFTP</li>
-          <li>CORS</li>
-        </ul>
+          <article className="tech-bubble shadow-bottom">
+            <h2 className="major-mono">Everything Else:</h2>
+            <ul>
+              <li>Phaser3</li>
+              <li>Git</li>
+              <li>GitHub</li>
+              <li>Vite</li>
+              <li>Visual Studio Code</li>
+              <li>Figma</li>
+              <li>Photoshop</li>
+              <li>Render</li>
+              <li>Wordpress</li>
+              <li>PANDAS</li>
+              <li>NumPY</li>
+              <li>Gulp</li>
+              <li>SSH/SFTP</li>
+              <li>CORS</li>
+            </ul>
+          </article>
+
+        </section>
       </div>
     </div>
   )

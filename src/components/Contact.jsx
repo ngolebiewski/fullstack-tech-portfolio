9c3a1240ee0aae1a0e3f222342bcb604b32bee53
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-08-15 14:52:26 -0400

    add icon from portfolio sites on render to kickstart services

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,6 +5,10 @@ const Contact = () => {
         <h1>Contact</h1>
         <p className="hacker-font blacklink"><a href="mailto:x@nickgolebiewski.com">x@nickgolebiewski.com</a> </p>
         <br />
+        <img src="https://keywordify.onrender.com/static/16bit-dot.png" alt="Keywordify dot" />
+        <img src="https://tower-of-zurpalen.onrender.com/assets/titlePage/warrior.png" alt="warrior from tower of zurpalen" width="auto" height="32px"/>
+        <img src="https://socket-sound-sharer.onrender.com/green-triangle-thing.png" alt="green triangle from sonic sound sharer" width="auto" height="32px"/>
+
       </div>
     </div>
   )
